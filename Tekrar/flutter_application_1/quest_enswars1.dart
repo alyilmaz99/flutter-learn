f95cfb5979387e49da0493f9c9a29d89f229a200
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-07-05 12:48:25 +0300

    quest_answers

---
+++ Tekrar/flutter_application_1/quest_enswars1.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  String name = "aly";
+  String lastName = "yilmaz";
+  int yearsOld = 23;
+
+  print(
+      "My name is $name $lastName , i am $yearsOld years old and my names lenght : ${name.length}");
+
+  int numberFirst = 10;
+  int numberSecond = 12;
+  int numberThird = 15;
+
+  print(
+      "Lenghts: $numberFirst, $numberSecond, $numberThird,  circumference : ${numberFirst + numberSecond + numberThird}");
+}
