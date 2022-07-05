0a462633db4b6189bb7c48066798b08670bbaf75
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-07-05 12:19:16 +0300

    var1

---
+++ Tekrar/flutter_application_1/variable1.dart
@@ -0,0 +1,20 @@
+//variables
+
+void main(List<String> args) {
+  int numberName = 10;
+  double doubleNumberName = 12.32;
+  num allNumberName = 1234; // for all numbers
+
+  String stringName = "test2";
+
+  bool boolName = true;
+
+  print(boolName);
+  print(stringName);
+  print(numberName);
+  print(allNumberName);
+  print("test");
+
+  var variableName = "buraya ne yazılırsa o değeri alır \'var\'";
+  print(variableName);
+}
