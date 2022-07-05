43f379ebbc66f6b5a195bca425b809fb13f8dc89
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-07-05 12:27:20 +0300

    var2

--- Tekrar/flutter_application_1/variable2.dart
+++ Tekrar/flutter_application_1/variable2.dart
@@ -1,1 +1,23 @@
+void main(List<String> args) {
+  int numberName = 10;
+  print(numberName);
 
+  numberName = 15;
+  print(numberName);
+
+  int converting = 15.5.toInt();
+  print(converting);
+/*
+  int nullSafety; // olduğu için bu hata verir null olarak tutamayız
+*/
+//eğer null kullanmamız gerekirse
+
+  double? nullName = null;
+
+  print(nullName);
+
+  //hexadecimal kullanım
+
+  int hexaName = 0xAABBCC;
+  print(hexaName);
+}
