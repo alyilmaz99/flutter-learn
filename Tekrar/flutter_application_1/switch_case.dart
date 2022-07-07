884373bbf06946201b0a92c8feb91ddd7aa692e5
alyilmaz99 <73197677+alyilmaz99@users.noreply.github.com>
2022-07-07 19:34:06 +0300

    switch

---
+++ Tekrar/flutter_application_1/switch_case.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  int numberFirst = 1;
+
+  switch (numberFirst) {
+    case 1:
+      print("1");
+      break;
+    case 2:
+      print("2");
+      break;
+    case 3:
+      print("3");
+      break;
+    default:
+      break;
+  }
+}
